77b118e756d2b70f20155f2b23f574974c076c04
A.Erkan ÇELİK <aerkanc@gmail.com>
2018-03-05 07:41:09 +0300

    card example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React App</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-);
+const Card = props => {
+  return (
+    <div style={{ margin: '1em' }}>
+      <img width="75" src={props.avatar_url} />
+      <div style={{ display: 'inline-block', marginLeft: '10px' }}>
+        <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>{props.name}</div>
+        <div>{props.company}</div>
+      </div>
+    </div>
+  );
+};
+let data = [
+  {
+    name: 'A.Erkan ÇELİK',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/1387120?v=4',
+    company: 'formalistech',
+  },
+  {
+    name: 'Enes Erdogan',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/6318361?v=4',
+    company: 'formalistech',
+  },
+];
+const CardList = props => {
+  return <div>{props.cards.map(card => <Card {...card} />)}</div>;
+};
+
+class Form extends React.Component {
+  render() {
+    return form;
+  }
+}
+
+ReactDOM.render(<CardList cards={data} />, mountNode);
 export default App;
